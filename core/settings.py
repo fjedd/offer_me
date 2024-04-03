89688caf98e2fa88a5a0422447ddebb6cf2107e6
fjed <d.ostapczuk1@gmail.com>
2024-04-03 15:31:52 +0200

    Add email tests

--- core/settings.py
+++ core/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_USERNAME")
+EMAIL_HOST_USER = os.getenv("EMAIL_USERNAME", "test@test.test")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
